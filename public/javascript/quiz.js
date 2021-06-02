4c6e34524609214c019de9c107b41a53bd1960eb
Andreas Wagner <andreasw.90@protonmail.com>
2021-06-02 18:59:15 +0200

    Check Funktion implementiert

--- public/javascript/quiz.js
+++ public/javascript/quiz.js
@@ -1,13 +1,37 @@
-function select(evt, exclusive){
-    if(exclusive==true){
-        for(let ele of document.querySelectorAll(".selected")){
+function select(evt, exclusive) {
+    if (exclusive == true) {
+        for (let ele of document.querySelectorAll(".selected")) {
             ele.classList.remove("selected");
             ele.classList.add("unselected")
         }
         evt.currentTarget.classList.add("selected");
         evt.currentTarget.classList.remove("unselected")
-    }else{
+    } else {
         evt.currentTarget.classList.add("selected");
         evt.currentTarget.classList.remove("unselected")
     }
+}
+
+function check() {
+    let trueAnswers = 0;
+    let points = 0;
+    for (let ele of document.querySelectorAll(".answer")) {
+        if (ele.classList.contains("true")) {
+            trueAnswers++;
+        }
+    }
+
+    for (let ele of document.querySelectorAll(".answer")) {
+        if (ele.classList.contains("true") && ele.classList.contains("selected")) {
+            points += 10 / trueAnswers;
+        } else if (ele.classList.contains("selected") && (points > 0)) {
+            if (points >= 10 / trueAnswers) {
+                points -= 10 / trueAnswers;
+            } else {
+                points = 0;
+            }
+        }
+    }
+    alert("Du hast " + points + " Punkte");
+
 }
